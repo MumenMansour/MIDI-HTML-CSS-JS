8500bf3554e2f70c30bb6267faa4b94874d529a3
Mumen Mansour <103611001+MumenMansour@users.noreply.github.com>
2022-07-06 17:43:55 -0300

    Update main.js

--- MID CSS-HTML-JS/main.js
+++ MID CSS-HTML-JS/main.js
@@ -64,6 +64,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function (evento){
+        console.log(evento.code)
+        if (evento.code === 'Sapace') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+        }
 }
+
 
 
