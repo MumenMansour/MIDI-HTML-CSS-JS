9cb32aa151ffafe1b2a2dda1fd6555ca88d76b5f
Mumen Mansour <mumenmansour97@gmail.com>
2022-07-06 15:36:30 -0300

    35

--- MID CSS-HTML-JS/main.js
+++ MID CSS-HTML-JS/main.js
@@ -5,17 +5,28 @@
 function tocasompom(){
     document.querySelector('#som_tecla_pom').play();
 }
-//assim o erro de auto execuçao da aplicação de "play" nao e barrada
-//pela proteçao de executaçao dos navegadores pois so sera execut
-//quando o usuario acionar o botao
+/*assim o erro de auto execuçao da aplicação de "play" nao e barrada
+pela proteçao de executaçao dos navegadores pois so sera execut
+quando o usuario acionar o botao
+
 document.querySelector('.tecla_pom').onclick = tocasompom;
-// ao atribuir o tocasom bom com o sinal de = para a funcao de onclic
-//o parenteses deve ser retirado para o navegador nao executar sem 
-// a interaçao do usuario causando assim o erro de permisao de autoexec
+
+ao atribuir o tocasom bom com o sinal de = para a funcao de onclic
+o parenteses deve ser retirado para o navegador nao executar sem 
+a interaçao do usuario causando assim o erro de permisao de autoexec
+desenvolvimento para fazer ativaçao do onclick para listas 
+para melhor desenvolvimento manutençao e ediçao*/
 
 
 
+/*---------------REFERENCIAS-------------------
+sao declaradas e criadas com base no valor que vao receber 
+e ter que armazenar 
 
+constante
+variavel
 
+constante --  nome da variavel --- recebe codigo */
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
