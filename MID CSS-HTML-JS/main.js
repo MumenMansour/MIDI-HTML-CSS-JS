da3518cbf7e9429be5444d67986f368c45437738
Mumen Mansour <mumenmansour97@gmail.com>
2022-07-06 15:16:30 -0300

    02

--- MID CSS-HTML-JS/main.js
+++ MID CSS-HTML-JS/main.js
@@ -1,1 +1,21 @@
-alert('ola mundo');
+//declaraçao de função 
+//declaraçao de parenteses em funçao e obrigatorio
+//tudo o que entrar dentro das chaves e o codigo que sera guardado e 
+//executado apenas quando ativado pela funçao
+function tocasompom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+//assim o erro de auto execuçao da aplicação de "play" nao e barrada
+//pela proteçao de executaçao dos navegadores pois so sera execut
+//quando o usuario acionar o botao
+document.querySelector('.tecla_pom').onclick = tocasompom;
+// ao atribuir o tocasom bom com o sinal de = para a funcao de onclic
+//o parenteses deve ser retirado para o navegador nao executar sem 
+// a interaçao do usuario causando assim o erro de permisao de autoexec
+
+
+
+
+
+
+
